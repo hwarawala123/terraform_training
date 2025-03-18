68331a5f87e89709324b3ce7ed547b6d501498aa
Warawala <Hussain.Warawala@saint-gobain.com>
2025-03-18 12:39:31 +0530

    vnet

--- main.tf
+++ main.tf
@@ -2,3 +2,10 @@ resource "azurerm_resource_group" "rg21" {
   name     = var.rg_name
   location = var.location
 }
+
+resource "azurerm_virtual_network" "vnet1" {
+  name = var.vnet_n
+  location = azurerm_resource_group.rg21.location
+  address_space = ["90.90.90.0/24"]
+  resource_group_name = azurerm_resource_group.rg21.name
+}
